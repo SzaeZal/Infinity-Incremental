bb884df54b214a95c89f9358d0d2b06ff0ddb6c9
SzaeZal <sza3zal@gmail.com>
2026-02-16 15:30:11 +0100

    added more menu buttons, you can toggle menu

--- src/components/MenuAndMap.vue
+++ src/components/MenuAndMap.vue
@@ -1,8 +1,22 @@
 <template>
-    <div class="menuButtons">
-        <div class="menuButton" :class="{'openMenuButton': isMenuOpen}" @click="ToggleMenu">
+    <div class="menuButtons" :class="{'menuButtonDisappear': isMenuOpen, 'menuButtonAppear' : !isMenuOpen}">
+        <div class="menuButton"  @click="ToggleMenu">
             <img src="./icons/menuOpen.png" alt="menu open" class="menuIcon openMenu">
             <p class="menuButtonText">Open Menu</p>
+        </div>
+    </div>
+    <div class="menuButtons" :class="{'menuButtonDisappear': !isMenuOpen, 'menuButtonAppear' : isMenuOpen}">
+        <div class="menuButton"  @click="ToggleMap">
+            <img src="./icons/MainMenuIcons/map.png" alt="map" class="menuIcon openMap">
+            <p class="menuButtonText">Map</p>
+        </div>
+        <div class="menuButton" @click="ToggleStats">
+            <img src="./icons/MainMenuIcons/stats.png" alt="menu close" class="menuIcon openStats">
+            <p class="menuButtonText">Stats</p>
+        </div>
+        <div class="menuButton" @click="ToggleMenu">
+            <img src="./icons/menuClose.png" alt="menu close" class="menuIcon closeMenu">
+            <p class="menuButtonText">Close Menu</p>
         </div>
     </div>
     
