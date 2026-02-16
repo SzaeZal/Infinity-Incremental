6a9a7e2122f38973e4887be954cd5b2bb47ef380
SzaeZal <sza3zal@gmail.com>
2026-02-16 18:14:17 +0100

    base settings layout

--- src/MenuComponents/SettingsMain.vue
+++ src/MenuComponents/SettingsMain.vue
@@ -1,29 +1,53 @@
 <template>
-    <div class="subMenu">
-        <div class="subMenuButton">
-            <svg
-                width="150px"
-                height="35px"
-                class="element subMenuSvg"
-            >
-                <path d="M20 2 L140 2 L130 33  L10 33 Z"  />
-                <text x="30" y="23">UI Settings</text>
-            </svg>
+    <div class="hasSubMenu">
+        <div class="subMenu">
+            <div class="subMenuButton" @click="ToggleUISettings">
+                <svg
+                    width="150px"
+                    height="35px"
+                    class="element subMenuSvg"
+                >
+                    <path d="M20 2 L140 2 L130 33  L10 33 Z"  />
+                    <text x="30" y="23">UI Settings</text>
+                </svg>
+            </div>
+            <div class="subMenuButton" @click="ToggleSaveSettings">
+                <svg
+                    width="150px"
+                    height="35px"
+                    class="element subMenuSvg"
+                >
+                    <path d="M20 2 L140 2 L130 33  L10 33 Z"  />
+                    <text x="20" y="23">Save Settings</text>
+                </svg>
+            </div>
         </div>
-        <div class="subMenuButton">
-            <svg
-                width="150px"
-                height="35px"
-                class="element subMenuSvg"
-            >
-                <path d="M20 2 L140 2 L130 33  L10 33 Z"  />
-                <text x="20" y="23">Save Settings</text>
-            </svg>
-            
+        <div class="subContent">
+            <UISettings v-show="isUISettingsToggled"/>
+            <SaveSettings v-show="isSaveSettingsToggled"/>
         </div>
     </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+import SaveSettings from './SettingsMenuComponents/SaveSettings.vue';
+import UISettings from './SettingsMenuComponents/UISettings.vue';
 
+const isSaveSettingsToggled=ref(false)
+const isUISettingsToggled=ref(false)
+
+const ToggleSaveSettings=()=>{
+    if(isSaveSettingsToggled.value==false){
+        isUISettingsToggled.value=false
+    }
+    isSaveSettingsToggled.value=!isSaveSettingsToggled.value
+}
+
+const ToggleUISettings=()=>{
+    if(isUISettingsToggled.value==false){
+        isSaveSettingsToggled=false
+    }
+    isUISettingsToggled.value=!isUISettingsToggled.value
+}
 </script>
--- src/MenuComponents/SettingsMenuComponents/UISettings.vue
+++ src/MenuComponents/SettingsMenuComponents/UISettings.vue
@@ -1,7 +1,16 @@
 <template>
+    <div class="settings">
+        <div class="setting">
 
+        </div>
+        <div class="setting">
+
+        </div>
+        <div class="setting">
+
+        </div>
+    </div>
 </template>
-
 <script setup>
 
 </script>
