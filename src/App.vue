0382f17bae001321cacaf1cff3e5f03cd9631268
SzaeZal <Sza3Zal@gmail.com>
2026-02-26 14:35:13 +0100

    moved navigation methods to positions

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <Positions class="coords" :navigation="playerStore.navigation" />
+  <Positions class="coords"/>
   <MainMenu />
   <DialogBox />
   <Points
@@ -188,107 +188,7 @@ const Load = () => {
 }
 
 Load()
-UpdateUIPositions()
+setInterval(UpdateUIPositions, 15)
 //#endregion
 
-//#region navigation
-let startX = 0,
-  startY = 0,
-  container = $('#app')
-
-const MouseDown = (e) => {
-  startX = e.clientX
-  startY = e.clientY
-
-  container.on('mousemove', MouseMove)
-}
-
-const MouseMove = (e) => {
-  playerStore.navigation.positionX += startX - e.clientX
-  playerStore.navigation.positionY += startY - e.clientY
-  startX = e.clientX
-  startY = e.clientY
-  UpdateUIPositions()
-}
-
-const MouseUp = () => {
-  container.off('mousemove')
-}
-
-container.on('mousedown', MouseDown)
-container.on('mouseup', MouseUp)
-
-const MoveDown = () => {
-  playerStore.navigation.positionY += 20
-}
-
-const MoveUp = () => {
-  playerStore.navigation.positionY -= 20
-}
-
-const MoveLeft = () => {
-  playerStore.navigation.positionX -= 20
-}
-
-const MoveRight = () => {
-  playerStore.navigation.positionX += 20
-}
-const activeMoveDirections = ref({
-  Down: false,
-  Up: false,
-  Left: false,
-  Right: false,
-})
-
-$(document).on('keydown', (e) => {
-  if (e.originalEvent.code == 'KeyS' || e.originalEvent.code == 'ArrowDown') {
-    activeMoveDirections.Down = true
-  }
-
-  if (e.originalEvent.code == 'KeyW' || e.originalEvent.code == 'ArrowUp') {
-    activeMoveDirections.Up = true
-  }
-
-  if (e.originalEvent.code == 'KeyA' || e.originalEvent.code == 'ArrowLeft') {
-    activeMoveDirections.Left = true
-  }
-
-  if (e.originalEvent.code == 'KeyD' || e.originalEvent.code == 'ArrowRight') {
-    activeMoveDirections.Right = true
-  }
-})
-
-$(document).on('keyup', (e) => {
-  if (e.originalEvent.code == 'KeyS' || e.originalEvent.code == 'ArrowDown') {
-    activeMoveDirections.Down = false
-  }
-
-  if (e.originalEvent.code == 'KeyW' || e.originalEvent.code == 'ArrowUp') {
-    activeMoveDirections.Up = false
-  }
-
-  if (e.originalEvent.code == 'KeyA' || e.originalEvent.code == 'ArrowLeft') {
-    activeMoveDirections.Left = false
-  }
-
-  if (e.originalEvent.code == 'KeyD' || e.originalEvent.code == 'ArrowRight') {
-    activeMoveDirections.Right = false
-  }
-})
-
-setInterval(() => {
-  if (activeMoveDirections.Up == true) MoveUp()
-  if (activeMoveDirections.Down == true) MoveDown()
-  if (activeMoveDirections.Left == true) MoveLeft()
-  if (activeMoveDirections.Right == true) MoveRight()
-  if (
-    activeMoveDirections.Up == true ||
-    activeMoveDirections.Down == true ||
-    activeMoveDirections.Left == true ||
-    activeMoveDirections.Right == true
-  ) {
-    UpdateUIPositions()
-  }
-}, 25)
-//#endregion
 </script>
--- src/components/Positions.vue
+++ src/components/Positions.vue
@@ -1,11 +1,102 @@
 <template>
     <div>
-        <p>X: {{ Math.floor(navigation.positionX /20) }}</p>
-        <p>Y: {{ Math.floor(navigation.positionY /20) }}</p>
+        <p>X: {{ Math.floor(playerStore.navigation.positionX /20) }}</p>
+        <p>Y: {{ Math.floor(playerStore.navigation.positionY /20) }}</p>
     </div>
 </template>
 <script setup>
-defineProps({
-    navigation: Object
+import { ref } from 'vue'
+import { usePlayerStore } from '@/stores/player'
+
+const playerStore=usePlayerStore()
+
+let startX = 0,
+  startY = 0,
+  container = $('#app')
+
+const MouseDown = (e) => {
+  startX = e.clientX
+  startY = e.clientY
+
+  container.on('mousemove', MouseMove)
+}
+
+const MouseMove = (e) => {
+  playerStore.navigation.positionX += startX - e.clientX
+  playerStore.navigation.positionY += startY - e.clientY
+  startX = e.clientX
+  startY = e.clientY
+}
+
+const MouseUp = () => {
+  container.off('mousemove')
+}
+
+container.on('mousedown', MouseDown)
+container.on('mouseup', MouseUp)
+
+const MoveDown = () => {
+  playerStore.navigation.positionY += 20
+}
+
+const MoveUp = () => {
+  playerStore.navigation.positionY -= 20
+}
+
+const MoveLeft = () => {
+  playerStore.navigation.positionX -= 20
+}
+
+const MoveRight = () => {
+  playerStore.navigation.positionX += 20
+}
+const activeMoveDirections = ref({
+  Down: false,
+  Up: false,
+  Left: false,
+  Right: false,
 })
+
+$(document).on('keydown', (e) => {
+  if (e.originalEvent.code == 'KeyS' || e.originalEvent.code == 'ArrowDown') {
+    activeMoveDirections.Down = true
+  }
+
+  if (e.originalEvent.code == 'KeyW' || e.originalEvent.code == 'ArrowUp') {
+    activeMoveDirections.Up = true
+  }
+
+  if (e.originalEvent.code == 'KeyA' || e.originalEvent.code == 'ArrowLeft') {
+    activeMoveDirections.Left = true
+  }
+
+  if (e.originalEvent.code == 'KeyD' || e.originalEvent.code == 'ArrowRight') {
+    activeMoveDirections.Right = true
+  }
+})
+
+$(document).on('keyup', (e) => {
+  if (e.originalEvent.code == 'KeyS' || e.originalEvent.code == 'ArrowDown') {
+    activeMoveDirections.Down = false
+  }
+
+  if (e.originalEvent.code == 'KeyW' || e.originalEvent.code == 'ArrowUp') {
+    activeMoveDirections.Up = false
+  }
+
+  if (e.originalEvent.code == 'KeyA' || e.originalEvent.code == 'ArrowLeft') {
+    activeMoveDirections.Left = false
+  }
+
+  if (e.originalEvent.code == 'KeyD' || e.originalEvent.code == 'ArrowRight') {
+    activeMoveDirections.Right = false
+  }
+})
+
+setInterval(() => {
+  if (activeMoveDirections.Up == true) MoveUp()
+  if (activeMoveDirections.Down == true) MoveDown()
+  if (activeMoveDirections.Left == true) MoveLeft()
+  if (activeMoveDirections.Right == true) MoveRight()
+}, 25)
 </script>
